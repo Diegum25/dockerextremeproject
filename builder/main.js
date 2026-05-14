ca5d71219d682df0f7478a1ffedf94152d992940
rollmein <diegoschacht25@gmail.com>
2026-05-14 15:09:32 -0600

    Kinda working builder
    Example of how to send a C file: curl -X POST -H "Content-Type: text/plain" -T main.c localhost:2167/
    
    I have no idea if this will work with the frontend

--- builder/main.js
+++ builder/main.js
@@ -1,19 +1,39 @@
+const execa = require('execa');
+
 const express = require('express')
 const app = express()
-const port = Number(process.env.EXTREME_BUILDER_PORT)
+var port = Number(process.env.EXTREME_BUILDER_PORT)
 
-var coolNumber = 0
+if (isNaN(port)){
+  console.error(`Port is NaN!\nSet EXTREME_BUILDER_PORT to something`)
+  console.log('Using port 2167')
 
-app.get('/', (req, res) => {
-  res.send(`Hello GET! ${coolNumber++}`)
-})
+  port = 2167
+}
 
-app.post('/', (req, res) => {
-  res.send(`Hello POST! ${coolNumber++}`)
-})
+app.use(express.text())
+
+app.post('/:filename', async(req, res) => {
+  const filename = req.params.filename;
+  console.log(`Receiving file: ${filename}`);
+
+  const gccanswer = await execa.execa("echo",[req.body]).pipe("gcc",["-x", "c", "-"]);
+
+  const outgcc = gccanswer.stdout
+  const errgcc = gccanswer.stderr
+
+  const execanswer = await execa.execa("./a.out");
+
+  const execout = execanswer.stdout
+  const execerr = execanswer.stderr
+
+  execa.execa("rm",["./a.out"]) // kinda nasty
+
+  console.log(execout,execerr);
+
+  res.send(`{"stdout": "${execout}","stderr": "${execerr}"}\n`);
+});
 
 app.listen(port, () => {
   console.log(`Builder listening on port ${port}`)
 })
-
-app.use('/cool',express.static('public'))
