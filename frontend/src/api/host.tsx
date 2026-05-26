2322b49e5a867e8c01a5eb5ccb9966750ece08d3
rollmein <diegoschacht25@gmail.com>
2026-05-25 19:48:54 -0600

    Removed /test
    it was not working lol

--- frontend/src/api/host.tsx
+++
@@ -1,9 +0,0 @@
-"use server"
-
-export async function InferHost(){ // dumb
-    if (process.env.EXTREME_HOST_DOCKER == '1'){
-        return "backend"
-    }else{
-        return "localhost"
-    }
-}
